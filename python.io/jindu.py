e2a255d5667ff97f8c60e216165c30988f50b018
cnzht <maxpda@hotmail.com>
2018-04-08 17:38:38 +0800

    example-xxx.py

---
+++ python.io/jindu.py
@@ -0,0 +1,14 @@
+﻿#-*-coding:utf-8-*-
+#str.py
+#进度条从1%-100%的程序，每隔0.1s刷新一次。
+import time as t
+jishu = 100
+print("{0:-^30}".format("执行开始"))
+for i in range (jishu+1):
+    a = ">"*i
+    b = "*"*(jishu-i)
+    c = (i/jishu)*100
+    ti = t.ctime() 
+    print("{:^3.0f}%  {}[{:<}>{:>}]".format(c,ti,a,b))
+    t.sleep(0.1)
+print("{0:-^30}".format("执行结束"))
