1e13fd36156c5f1541f17ab3d52dee68282ae56b
cnzht <maxpda@hotmail.com>
2018-07-31 20:57:14 +0800

    180731review

---
+++ c/jianzhioffer/jianzhioffer/digui.c
@@ -0,0 +1,21 @@
+#include "OftenInclude.h"
+int func(int );
+int main()
+{
+	printf("%d", func(5));
+	getchar();
+	return 0;
+}
+int func(int n)
+{
+	int m = 1;
+	if (n == 0 || n == 1)
+	{
+		m = 1;
+	}
+	else
+	{
+		m = n*func(n - 1);
+	}
+	return m;
+}
