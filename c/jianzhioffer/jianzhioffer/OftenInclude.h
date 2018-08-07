8119ee283e6c9a75e9242e7de766fa1ec908525f
cnzht <maxpda@hotmail.com>
2018-08-07 09:55:36 +0800

    20180806提交

--- c/jianzhioffer/jianzhioffer/OftenInclude.h
+++ c/jianzhioffer/jianzhioffer/OftenInclude.h
@@ -1,5 +1,8 @@
+#ifndef _OFTENINCLUDE_
+#define	_OFTENINCLUDE_
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
-#include <time.h>
 #include <string.h>
+#include <time.h>
 #include <math.h>
+#endif
